83f002026c6788423f9f5a89d6f21da01c44b39f
Katie Reed <katharynreedpierce@gmail.com>
2017-02-10 09:56:34 -0800

    add new contact posting abilities

--- app/app.php
+++ app/app.php
@@ -15,7 +15,17 @@
         return $app['twig']->render('index.html.twig', array('contacts' => Contact::getAll()));
     });
 
-    
+    $app->post("/add", function() use ($app) {
+        return $app['twig']->render('add.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+    $app->post("/submit", function() use ($app) {
+        $new_contact = new Contact(ucfirst(strtolower($_POST['name'])), ucfirst(strtolower($_POST['address'])), $_POST['phone']);
+        $new_contact->save();
+        return $app['twig']->render('index.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+
 
     return $app;
 ?>
